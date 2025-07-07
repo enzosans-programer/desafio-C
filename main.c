023ecbb4d526647a257d17f323d904306a601de0
Enzo <enzosl_350@outlook.com>
2025-07-07 15:28:06 -0300

    push fiz a intro do jogo

--- main.c
+++ main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
+#include <windows.h>
+#include <mmsystem.h>
+#pragma comment(lib, "winmm.lib")  // Vincula a biblioteca
 
 int main(int argc, char *argv[]) {
-
-printf("hello world!\n");
-
+    printf("\n");
+    PlaySoundA("robots_and_aliens-joel_cummins.wav", NULL, SND_FILENAME | SND_ASYNC);
+    return 0;
 }
---
+++ maino.c
@@ -0,0 +1,32 @@
+#include <windows.h>
+#include <mmsystem.h>
+#include <stdio.h>
+#include <stdint.h>
+
+int main() {
+    const char *arquivo = "C:\\Users\\enzos\\OneDrive\\Documentos\\Gravacoes de som\\videoplayback.wav"; // caminho absoluto do .wav
+int i;
+
+    PlaySound(arquivo, NULL, SND_FILENAME | SND_ASYNC);
+    printf("feito por ENZO...\n");
+	printf("\n");
+	sleep(4);
+	printf("Enzotubers Memezado edicoes apresenta...\n");
+	printf("\n");
+	sleep(5);
+	printf("A JORNADA POR CLANDOR\n");
+	sleep(2);
+	for(i = 1;i <= 9; i++){
+	printf("\n");
+	printf("\n");
+	printf("\n");
+}
+	sleep(1);
+	for(i = 1;i <= 9; i++){
+	printf("\n");
+	printf("\n");
+	printf("\n");
+}
+	sleep(1225);
+    return 0;
+}
