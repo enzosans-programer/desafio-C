47d3720637ac8d74bd598effff9c7cabea19e68d
Enzo <enzosl_350@outlook.com>
2025-07-09 14:30:55 -0300

    pull: adicionei pagina para adicionar o nome do usuario

--- maino.c
+++ maino.c
@@ -2,13 +2,36 @@
 #include <mmsystem.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 
-int main() {
-    const char *arquivo = "C:\\Users\\enzos\\OneDrive\\Documentos\\Gravacoes de som\\videoplayback.wav"; // caminho absoluto do .wav
-int i;
 
-    PlaySound(arquivo, NULL, SND_FILENAME | SND_ASYNC);
-    printf("feito por ENZO...\n");
+typedef struct {
+    char nome[50];
+    int vida, ataque, defesa;
+    int xp, nivel, ouro;
+}   Usuario;
+
+typedef struct {
+    int vida, ataque, defesa, xp;
+} Monstro;
+int main(){
+int i, loop, h;
+Usuario jogador;
+for(loop = 1;loop == 1;){
+PlaySound("C:\\Users\\enzos\\OneDrive\\Documentos\\Gravacoes de som\\musicadeespera.wav", NULL, SND_FILENAME | SND_ASYNC);
+printf("digite seu nome: ");
+scanf(" %[^\n]s", jogador.nome);
+printf("\n");
+printf("%s esse e seu nome 1sim 2nao\n", jogador.nome);
+scanf("%d", &h);
+if(h == 1){
+	printf("comecando o jogo...\n");
+	sleep(5);
+	printf("\n");printf("\n");printf("\n");printf("\n");printf("\n");printf("\n");
+	break;
+}
+}
+    PlaySound("C:\\Users\\enzos\\OneDrive\\Documentos\\Gravacoes de som\\videoplayback.wav", NULL, SND_FILENAME | SND_ASYNC);	printf("feito por ENZO...\n");
 	printf("\n");
 	sleep(4);
 	printf("Enzotubers Memezado edicoes apresenta...\n");
@@ -27,6 +50,28 @@ int i;
 	printf("\n");
 	printf("\n");
 }
-	sleep(1225);
-    return 0;
+sleep(3);
+printf("a muito tempo existiam duas formas de vida humanos e monstros...\n");
+printf("     _____\n");
+printf(" O  |     |\n");
+printf("/|\\ |O  O |\n");
+printf("/ \\ |_____|\n");
+sleep(3);
+printf("elas viviam em harmonia ate que uma enorme guerra separou o continente em 2\n");
+printf("       ______  \n");
+printf("       \\    / \n");	
+printf("       /    \\ \n");
+printf("\\      |    |       /\n");
+printf(" \\ \\   \\    /    / /\n");
+printf("  \\ \\ \\ \\__/  / / /\n");
+sleep(3);
+printf("\n");
+printf("entre os dois pedacos a um enorme mar chamado O DESCONHECIDO...\n");
+printf("    _    _     _ \n");
+printf("   /    /     /  \n");
+printf("___\\____\\_____\\__\n");
+sleep(6);
+printf("\n");printf("\n");printf("\n");printf("\n");printf("\n");printf("\n");printf("\n");printf("\n");printf("\n");printf("\n");printf("\n");printf("\n");printf("\n");printf("\n");printf("\n");printf("\n");printf("\n");printf("\n");printf("\n");printf("\n");printf("\n");printf("\n");printf("\n");printf("\n");printf("\n");printf("\n");printf("\n");printf("\n");printf("\n");printf("\n");printf("\n");printf("\n");printf("\n");printf("\n");printf("\n");printf("\n");
+    
+	return 0;
 }
