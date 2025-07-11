2df34c6e45a765cb5fac34e0a2eb9e4c24914dcb
Enzo <enzosl_350@outlook.com>
2025-07-11 11:25:55 -0300

    feat: adicionado texto colaridos de cores diferentes inteiros ou separadamente e melhorias na intro do jogo

--- maino.c
+++ maino.c
@@ -7,7 +7,7 @@
 
 typedef struct {
     char nome[50];
-    int vida, ataque, defesa;
+    int vida, ataque, defesa, sexo;
     int xp, nivel, ouro;
 }   Usuario;
 
@@ -44,6 +44,20 @@ int main(){
 	int i, loop, h;
 	Usuario jogador;
 	
+
+
+const char *vermelho = "\033[31m";
+const char *amarelo = "\033[33m";
+const char *azul = "\033[34m";
+const char *verde = "\033[32m";
+const char *reset = "\033[0m";
+const char *laranja = "\033[91m";
+const char *verdeclaro = "\033[92m";
+const char *azulclaro = "\033[94m";
+const char *rosa = "\033[95m";
+const char *roxo = "\033[35m";
+	
+//printf("%steste%steste%steste%steste%steste%steste%steste%steste%steste%s\n", vermelho, laranja, amarelo, verdeclaro, verde, azulclaro, azul, rosa, roxo, reset);	
 	for(loop = 1;loop == 1;){
 		//Chama a função de tocar musica
 		tocarMusica("sounds\\elevador.wav");
@@ -63,18 +77,26 @@ int main(){
 	
 	tocarMusica("sounds\\intro.wav");
     
-	printf("feito por ENZO...\n");
+	printf("feito por %sENZO%s...\n", azul, reset);
 	printf("\n");
 	
 	sleep(4);
 	
-	printf("Enzotubers Memezado edicoes apresenta...\n");
+	printf("%sEnzotubers Memezado%s edicoes apresenta...\n", azul, reset);
 	printf("\n");
 	
 	sleep(5);
 	
-	printf("A JORNADA POR CLANDOR\n");
-	
+	printf("AAAAAA      jjjjjjjj OOOOOO RRRRR  NN  N AAAAAA DDDDD  AAAAA\n");
+	printf("A    A         jj    O    O R   R  N N N A    A D    D A   A\n");
+	printf("A    A       jjjj    OOOOOO R   RR N  NN A    A DDDDDD A   A\n");
+	printf("					PPPP  OOOOO RRRRR\n");
+	printf("					P___P O   O R   R\n");
+	printf("					P     OOOOO R   RR\n");
+	printf("%sCCCCCC LL     AAAAAAAA NNNN  NN DDDDD   OOOOOOO RRRRR\n", amarelo);
+	printf("%sCC     LL     AA    AA NN NN NN DD   DD OO   OO RR   R\n", amarelo);
+	printf("%sCC     LL     AAAAAAAA NN  NNNN DD   DD OO   OO RRRRRR\n", amarelo);
+	printf("%sCCCCCC LLLLLL AA    AA NN   NNN DDDDD   OOOOOOO RR  RRR\n", amarelo);
 	sleep(2);
 	
 	for(i = 1;i <= 9; i++){
@@ -85,7 +107,7 @@ int main(){
 	
 	sleep(1);
 	
-	for(i = 1;i <= 9; i++){
+	for(i = 1;i <= 9; i++){ 
 		printf("\n");
 		printf("\n");
 		printf("\n");
@@ -119,6 +141,7 @@ int main(){
 	sleep(6);
 	
 	printf("\n");printf("\n");printf("\n");printf("\n");printf("\n");printf("\n");printf("\n");printf("\n");printf("\n");printf("\n");printf("\n");printf("\n");printf("\n");printf("\n");printf("\n");printf("\n");printf("\n");printf("\n");printf("\n");printf("\n");printf("\n");printf("\n");printf("\n");printf("\n");printf("\n");printf("\n");printf("\n");printf("\n");printf("\n");printf("\n");printf("\n");printf("\n");printf("\n");printf("\n");printf("\n");printf("\n");
-    
+    printf("mais um humano chamado %s%s%s muitos anos depois decidiu ir atras dos monstros para derrotalos um por um...\n", vermelho, jogador.nome, reset);
+    sleep(6);
 	return 0;
 }
